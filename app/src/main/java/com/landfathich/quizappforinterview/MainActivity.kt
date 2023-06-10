dbbe02675fa750299b5de51211a0300b702e84e8
Arthur Kozhemyakin <trable228@icloud.com>
2023-06-10 15:46:15 +0300

    xml for landing screen

--- app/src/main/java/com/landfathich/quizappforinterview/MainActivity.kt
+++ app/src/main/java/com/landfathich/quizappforinterview/MainActivity.kt
@@ -2,10 +2,13 @@ package com.landfathich.quizappforinterview
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.landfathich.quizappforinterview.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater).also { setContentView(it.root) }
     }
 }
