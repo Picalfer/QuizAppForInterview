1f93e24507a7d8f301a5f0ffe5dcd11d145178f5
Arthur Kozhemyakin <trable228@icloud.com>
2023-06-10 18:36:44 +0300

    question activity work

--- app/src/main/java/com/landfathich/quizappforinterview/Constants.kt
+++ app/src/main/java/com/landfathich/quizappforinterview/Constants.kt
@@ -2,7 +2,7 @@ package com.landfathich.quizappforinterview
 
 object Constants {
 
-    fun getQuestions() :ArrayList<Question> {
+    fun getQuestions(): ArrayList<Question> {
         val questionList = ArrayList<Question>()
 
         //1
@@ -12,9 +12,43 @@ object Constants {
             R.drawable.maktub,
             R.drawable.voina_i_mir,
             R.drawable.portre_doriana,
-            R.drawable.voina_i_mir
+            2
         )
         questionList.add(que1)
+
+        //2
+        val que2 = Question(
+            2,
+            "Если ты знаешь, что человек никогда не будет твоим, то любить его можно бесконечно долго.",
+            R.drawable.voina_i_mir,
+            R.drawable.maktub,
+            R.drawable.portre_doriana,
+            3
+        )
+        questionList.add(que2)
+
+
+        //3
+        val que3 = Question(
+            3,
+            "Знаешь, когда мы окончательно теряем дорогих нам людей? Когда больше не ощущаем боли от их потери.",
+            R.drawable.maktub,
+            R.drawable.voina_i_mir,
+            R.drawable.prejde_chem,
+            3
+        )
+        questionList.add(que3)
+
+        //4
+        val que4 = Question(
+            4,
+            "Не имеет значения, что думают другие – поскольку они в любом случае что-нибудь подумают. Так что расслабься.",
+            R.drawable.maktub,
+            R.drawable.prejde_chem,
+            R.drawable.portre_doriana,
+            1
+        )
+        questionList.add(que4)
 
         return questionList
     }
--- app/src/main/java/com/landfathich/quizappforinterview/QuizQuestionsActivity.kt
+++ app/src/main/java/com/landfathich/quizappforinterview/QuizQuestionsActivity.kt
@@ -1,15 +1,140 @@
 package com.landfathich.quizappforinterview
 
+import android.graphics.Color
+import android.graphics.Typeface
+import android.media.Image
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.view.View
+import android.view.View.OnClickListener
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.core.content.ContextCompat
+import com.landfathich.quizappforinterview.databinding.ActivityQuizQuestionsBinding
 
-class QuizQuestionsActivity : AppCompatActivity() {
+class QuizQuestionsActivity : AppCompatActivity(), OnClickListener {
+
+    private lateinit var binding: ActivityQuizQuestionsBinding
+
+    private var mCurrentPosition: Int = 1
+    private var mQuestionsList: ArrayList<Question>? = null
+    private var mSelectedOptionPosition: Int = 0
+    private var mCorrectAnswers: Int = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_quiz_questions)
+        binding =
+            ActivityQuizQuestionsBinding.inflate(layoutInflater).also { setContentView(it.root) }
 
-        val questionsList = Constants.getQuestions()
-        Log.i("Questions size", "${questionsList.size}")
+        mQuestionsList = Constants.getQuestions()
+        Log.i("Questions size", "${mQuestionsList!!.size}")
+
+        setQuestion()
+
+        binding.apply {
+            option1.setOnClickListener(this@QuizQuestionsActivity)
+            option2.setOnClickListener(this@QuizQuestionsActivity)
+            option3.setOnClickListener(this@QuizQuestionsActivity)
+            btnNext.setOnClickListener(this@QuizQuestionsActivity)
+        }
+    }
+
+    private fun setQuestion() {
+        val question = mQuestionsList!![mCurrentPosition - 1]
+
+        defaultOptionsView()
+
+        binding.apply {
+            tvQuestion.text = question.question
+            option1.setImageResource(question.imageOne)
+            option2.setImageResource(question.imageTwo)
+            option3.setImageResource(question.imageThree)
+        }
+    }
+
+    private fun defaultOptionsView() {
+        val options = ArrayList<ImageView>()
+        options.add(0, binding.option1)
+        options.add(1, binding.option2)
+        options.add(2, binding.option3)
+
+        for (option in options) {
+            option.background = ContextCompat.getDrawable(
+                this,
+                R.drawable.default_option_border_bg
+            )
+        }
+    }
+
+    override fun onClick(view: View) {
+        when (view.id) {
+            R.id.option1 -> {
+                selectedOptionView(binding.option1, 1)
+            }
+
+            R.id.option2 -> {
+                selectedOptionView(binding.option2, 2)
+            }
+
+            R.id.option3 -> {
+                selectedOptionView(binding.option3, 3)
+            }
+
+            R.id.btn_next -> {
+                if (mSelectedOptionPosition == 0) {
+                    mCurrentPosition++
+
+                    when {
+                        mCurrentPosition <= mQuestionsList!!.size -> {
+                            setQuestion()
+                        }
+
+                        else -> {
+                            Toast.makeText(
+                                this,
+                                "You have successfully completed the Quiz",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        }
+                    }
+                } else {
+                    val question = mQuestionsList?.get(mCurrentPosition - 1)
+                    if (question!!.correctImage != mSelectedOptionPosition) {
+                        answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    } else {
+                        mCorrectAnswers++
+                    }
+                    answerView(question.correctImage, R.drawable.correct_option_border_bg)
+
+                    mSelectedOptionPosition = 0
+                }
+            }
+        }
+    }
+
+    private fun answerView(answer: Int, drawableView: Int) {
+        when (answer) {
+            1 -> {
+                binding.option1.background = ContextCompat.getDrawable(this, drawableView)
+            }
+
+            2 -> {
+                binding.option2.background = ContextCompat.getDrawable(this, drawableView)
+            }
+
+            3 -> binding.option3.background = ContextCompat.getDrawable(this, drawableView)
+        }
+    }
+
+
+    private fun selectedOptionView(iv: ImageView, selectedOptionNum: Int) {
+        defaultOptionsView()
+        mSelectedOptionPosition = selectedOptionNum
+
+        iv.background = ContextCompat.getDrawable(
+            this,
+            R.color.yellow
+        )
     }
 }
